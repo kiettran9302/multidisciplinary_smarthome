8249dc5a1b33e66e86a4a13508545204051477a8
Phuc Nguyen Nho Gia <nngiaphuc0101@gmail.com>
2023-03-12 20:05:54 +0700

    complete list view with 4 rooms

--- lib/components/room_box.dart
+++ lib/components/room_box.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
-class RoomBox extends StatelessWidget {
+class RoomBox extends StatefulWidget {
   final IconData roomIcon;
   final String roomName;
-  const RoomBox({Key? key, required this.roomIcon, required this.roomName})
+  bool isActive;
+
+  RoomBox(
+      {Key? key,
+      required this.roomIcon,
+      required this.roomName,
+      required this.isActive})
       : super(key: key);
 
+  @override
+  _RoomBoxState createState() => _RoomBoxState();
+}
+
+class _RoomBoxState extends State<RoomBox> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,20 +27,25 @@ class RoomBox extends StatelessWidget {
           Container(
             alignment: Alignment.center,
             decoration: BoxDecoration(
-                border: Border.all(width: 2),
-                borderRadius: BorderRadius.circular(20)),
+              border: Border.all(width: 2),
+              borderRadius: BorderRadius.circular(20),
+              color: widget.isActive ? Colors.black : Colors.white,
+            ),
             height: 100.0,
             width: 100,
             child: Icon(
-              roomIcon,
+              widget.roomIcon,
+              color: widget.isActive ? Colors.white : Colors.black,
               size: 40,
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(
-              roomName,
-              style: const TextStyle(fontSize: 14.0),
+              widget.roomName,
+              style: const TextStyle(
+                fontSize: 14.0,
+              ),
             ),
           ),
         ],
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import '../components/room_box.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<bool> isActiveList = [true, false, false, false];
+  void updateIsActive(int index) {
+    setState(() {
+      if (isActiveList[index] != true) {
+        isActiveList[index] = true;
+      }
+      for (int i = 0; i < isActiveList.length; i++) {
+        if (i != index) {
+          isActiveList[i] = false;
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +47,33 @@ class HomePage extends StatelessWidget {
                     const Text("Welcome to your smart home"),
                   ],
                 ),
-                const Icon(Icons.person_outline)
+                GestureDetector(
+                    onTap: () => print("clicked icon"),
+                    child: const Icon(Icons.person_outline))
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 40, bottom: 5, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Room selection",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                GestureDetector(
+                  // placeholder
+                  child: Icon(Icons.more_horiz),
+                  onTap: () => print("Clicked on here"),
+                )
               ],
             ),
           ),
           const Padding(
-            padding: EdgeInsets.only(top: 40, bottom: 5, left: 20),
-            child: Text(
-              "Room selection",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Divider(
+              thickness: 1.5,
             ),
           ),
           Padding(
@@ -45,15 +82,66 @@ class HomePage extends StatelessWidget {
               height: 150,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: const [
-                  RoomBox(roomIcon: Icons.abc, roomName: "Living Room"),
-                  RoomBox(roomIcon: Icons.bed, roomName: "Bedroom"),
-                  RoomBox(roomIcon: Icons.kitchen, roomName: "Kitchen"),
-                  RoomBox(roomIcon: Icons.garage, roomName: "Garage")
+                children: [
+                  GestureDetector(
+                    onTap: () => updateIsActive(0),
+                    child: RoomBox(
+                      roomIcon: Icons.living,
+                      roomName: "Living Room",
+                      isActive: isActiveList[0],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => updateIsActive(1),
+                    child: RoomBox(
+                      roomIcon: Icons.bed,
+                      roomName: "Bedroom",
+                      isActive: isActiveList[1],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => updateIsActive(2),
+                    child: RoomBox(
+                      roomIcon: Icons.dining,
+                      roomName: "Dining Room",
+                      isActive: isActiveList[2],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => updateIsActive(3),
+                    child: RoomBox(
+                      roomIcon: Icons.garage,
+                      roomName: "Garage",
+                      isActive: isActiveList[3],
+                    ),
+                  ),
                 ],
               ),
             ),
-          )
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 5, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Devices",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                GestureDetector(
+                  // placeholder
+                  child: Icon(Icons.more_horiz),
+                  onTap: () => print("Clicked on here"),
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Divider(
+              thickness: 1.5,
+            ),
+          ),
 
           // devices
         ]),
